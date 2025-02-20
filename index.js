11bfced8a02fc2c213eb1e60dbb00b3beb126c45
Juan P Celedon C <44074781+juanceledon@users.noreply.github.com>
2025-02-20 17:20:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 const { google } = require("googleapis");
-const fs = require("fs");
-
-const credentials = {
-  installed: {
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    redirect_uris: [process.env.REDIRECT_URI],
-  },
-};
-
-const tokens = {
-  refresh_token: process.env.REFRESH_TOKEN,
-};
 
 const oauth2Client = new google.auth.OAuth2(
-  credentials.installed.client_id,
-  credentials.installed.client_secret,
-  credentials.installed.redirect_uris[0]
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
 );
-
 const updateVideo = async () => {
-  oauth2Client.setCredentials(tokens);
+
+  oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
   const youtube = google.youtube({
     version: "v3",
@@ -29,26 +16,34 @@ const updateVideo = async () => {
 
   try {
     const result = await youtube.videos.list({
-      id: "ebAAYj7HWWY", // <-- ID
+      id: "pg_vngerTiU",
       part: "statistics,snippet",
     });
 
     if (result.data.items.length > 0) {
       const stats = result.data.items[0].statistics;
 
+      const newTitle = `Este video tiene ${stats.viewCount} vistas, ¿cómo lo sé?`;
+
+      const currentTitle = result.data.items[0].snippet.title;
+
+      if (currentTitle === newTitle) {
+        return;
+      }
+
       await youtube.videos.update({
         part: "snippet",
         requestBody: {
-          id: "ebAAYj7HWWY", // <-- ID
+          id: "pg_vngerTiU",
           snippet: {
-            title: `This video has ${stats.viewCount} views`,
+            title: newTitle,
             categoryId: 28,
           },
         },
       });
     }
   } catch (error) {
-    console.error("❌ Error:", error);
+    console.error(error);
   }
 };
 
